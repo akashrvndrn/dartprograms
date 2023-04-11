e4eca8ccd6d941eb3b69ea914560c5ad19e4392e
akashrvndrn <akashraveendranv4@gmail.com>
2023-04-11 13:40:31 +0530

    helo

---
+++ lib/string opertions.dart
@@ -0,0 +1,10 @@
+void main(){
+String animal=" tiger";
+print(animal.length);
+String bird="crow";
+print(animal.compareTo(bird)==0);
+
+bird.toLowerCase();
+print(animal.toUpperCase());
+print(animal.trim());
+}
