2094d8988d8f4d3883cdad87e4b5904b5c481549
akashrvndrn <akashraveendranv4@gmail.com>
2023-04-11 11:48:06 +0530

    helo

---
+++ bin/dartprograms.dart
@@ -0,0 +1,23 @@
+void main(){
+  String name="Akash";//Here String is the data type and name is variable name and this is single line comment
+  print(name);
+/*hfjlhj
+jdhfj
+ */
+  //this is multiline comment
+  int b=10;
+  print(b);
+  double a=10.1;
+  print(a);
+  String nam="hello world";
+      print(nam);
+
+      print("My name is ${name} and my rollno is ${b} ");
+
+      bool isValid=true;
+print(isValid);
+List item=[1,2,3];
+print(item);
+List items=["akash","hello","world"];
+print(items); 
+}
---
+++ lib/string_operations.dart
@@ -0,0 +1,10 @@
+void main(){
+
+String a="apple";
+String b="ball";
+print(a.length);
+print(b.length);
+print(a.compareTo(b)==0);
+String concat=a+b;
+print(concat);
+}
