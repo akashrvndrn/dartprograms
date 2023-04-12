4c42f423d739afad896edf0ca9bc28c5c7cd6460
akashrvndrn <akashraveendranv4@gmail.com>
2023-04-12 11:55:24 +0530

    helo

---
+++ lib/operators/Assignmentoperator.dart
@@ -0,0 +1,16 @@
+void main(){
+  var A=50;
+  var B=30;
+  var C=A+=B;
+  print(C);
+  var D=A-=B;
+  print(D);
+  var E=A*=B;
+  print(D);
+  double x=36;
+  double y=74;
+  double F=x/=y;
+  print(F);
+  var G=A%=B;
+  print(G);
+}
