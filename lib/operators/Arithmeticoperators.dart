a3d3feb415983d6f8068ff2441f6b3bf8a562b1c
akashrvndrn <akashraveendranv4@gmail.com>
2023-04-12 11:19:28 +0530

    helo

---
+++ lib/operators/Arithmeticoperators.dart
@@ -0,0 +1,15 @@
+void main(){
+  int num1=8;
+  int num2=9;
+
+  print("Addition");
+  print("num1+num2=${num1+num2}");
+  print("subtraction");
+  print("num1-num2=${num1-num2}");
+  print("multiplication");
+  print("num1*num2=${num1*num2}");
+  print("modulus");
+  print("num1%num2=${num1%num2}");
+  print("division");
+  print("num1/num2=${num1/num2}");
+}
